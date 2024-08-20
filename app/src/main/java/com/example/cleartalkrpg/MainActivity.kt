26fff063241e4c6857d999842c43c886a8e2fb82
nashdev255 <nashdev255@gmail.com>
2024-08-20 18:25:40 +0900

    Add component TitleScreen

--- app/src/main/java/com/example/cleartalkrpg/MainActivity.kt
+++ app/src/main/java/com/example/cleartalkrpg/MainActivity.kt
@@ -10,6 +10,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.cleartalkrpg.titlescreen.TitleScreen
 import com.example.cleartalkrpg.ui.theme.ClearTalkRPGTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,30 +18,24 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ClearTalkRPGTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+                TitleScreen(name = "android")
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Good morning $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ClearTalkRPGTheme {
-        Greeting("test")
-    }
-}
+//@Composable
+//fun Greeting(name: String, modifier: Modifier = Modifier) {
+//    Text(
+//        text = "Good morning $name!",
+//        modifier = modifier
+//    )
+//}
+//
+//@Preview(showBackground = true)
+//@Composable
+//fun GreetingPreview() {
+//    ClearTalkRPGTheme {
+//        Greeting("test")
+//    }
+//}
--- app/src/main/java/com/example/cleartalkrpg/titlescreen/TitleScreen.kt
+++ app/src/main/java/com/example/cleartalkrpg/titlescreen/TitleScreen.kt
@@ -1,2 +1,12 @@
 package com.example.cleartalkrpg.titlescreen
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun TitleScreen(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Good evening $name!",
+        modifier = modifier
+    )
+}
