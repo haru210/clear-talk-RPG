4c14a3595fedf039f8d27a498c81a60d171e78f5
nashdev255 <nashdev255@gmail.com>
2024-08-21 12:36:41 +0900

    Delete wasted code

--- app/src/main/java/com/example/cleartalkrpg/MainActivity.kt
+++ app/src/main/java/com/example/cleartalkrpg/MainActivity.kt
@@ -3,13 +3,6 @@ package com.example.cleartalkrpg
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.cleartalkrpg.titlescreen.TitleScreen
 import com.example.cleartalkrpg.ui.theme.ClearTalkRPGTheme
 
@@ -18,24 +11,8 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ClearTalkRPGTheme {
-                TitleScreen(name = "android")
+                TitleScreen(name = "Android")
             }
         }
     }
 }
-
-//@Composable
-//fun Greeting(name: String, modifier: Modifier = Modifier) {
-//    Text(
-//        text = "Good morning $name!",
-//        modifier = modifier
-//    )
-//}
-//
-//@Preview(showBackground = true)
-//@Composable
-//fun GreetingPreview() {
-//    ClearTalkRPGTheme {
-//        Greeting("test")
-//    }
-//}
