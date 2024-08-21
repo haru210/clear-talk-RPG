d5a610e5a7c6ce8c5d616e6b07ea903acff00b1d
nashdev255 <nashdev255@gmail.com>
2024-08-21 12:38:10 +0900

    Add TitleScreen component Preview

--- app/src/main/java/com/example/cleartalkrpg/titlescreen/TitleScreen.kt
+++ app/src/main/java/com/example/cleartalkrpg/titlescreen/TitleScreen.kt
@@ -2,11 +2,24 @@ package com.example.cleartalkrpg.titlescreen
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.material3.Text
+import androidx.compose.ui.unit.dp
+import androidx.compose.foundation.layout.padding
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.cleartalkrpg.ui.theme.ClearTalkRPGTheme
 
 @Composable
-fun TitleScreen(name: String, modifier: Modifier = Modifier) {
+fun TitleScreen(name: String) {
     Text(
         text = "Good evening $name!",
-        modifier = modifier
+        modifier = Modifier.padding(24.dp)
     )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun TitleScreenPreview() {
+    ClearTalkRPGTheme {
+        TitleScreen("test")
+    }
 }
