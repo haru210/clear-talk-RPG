0f6c574024f86b51d026170afe3a0b879306f1b8
nashdev255 <nashdev255@gmail.com>
2024-08-22 14:15:18 +0900

    Add ViewResultHistoryButton

--- app/src/main/java/com/example/cleartalkrpg/titlescreen/TitleScreen.kt
+++ app/src/main/java/com/example/cleartalkrpg/titlescreen/TitleScreen.kt
@@ -2,8 +2,9 @@ package com.example.cleartalkrpg.titlescreen
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -12,7 +13,6 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Alignment
-import androidx.compose.ui.draw.blur
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
@@ -21,12 +21,13 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
 import com.example.cleartalkrpg.R
 import com.example.cleartalkrpg.ui.theme.ClearTalkRPGTheme
+import com.example.cleartalkrpg.ui.theme.HistoryIcon
 
 @Composable
 fun TitleScreen() {
     TitleScreenBackgroundImage()
     TitleLogo()
-    TapToStartButton()
+    TitleScreenMenu()
 }
 
 @Composable
@@ -64,24 +65,56 @@ fun TitleLogo() {
 }
 
 @Composable
-fun TapToStartButton() {
+fun TitleScreenMenu() {
     Box(
         contentAlignment = Alignment.Center,
-        modifier = Modifier.fillMaxSize()
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(0.dp, 120.dp, 0.dp, 0.dp)
     ) {
-        Surface(
-            color = Color.Gray.copy(alpha = 0.65f),
-            modifier = Modifier
-                .align(Alignment.Center)
-                .padding(0.dp, 150.dp, 0.dp, 0.dp)
+        Row(
+            horizontalArrangement = Arrangement.spacedBy(20.dp)
         ) {
-            Text(
-                text = "tap to start",
-                fontWeight = FontWeight.Bold,
-                fontSize = 20.sp,
-                color = Color.White,
-                modifier = Modifier.padding(36.dp, 12.dp)
-            )
+            TapToStartButton()
+            ViewResultHistoryButton()
+        }
+    }
+}
+
+@Composable
+fun TapToStartButton() {
+    Surface(
+        color = Color.Gray.copy(alpha = 0.65f)
+    ) {
+        Text(
+            text = "tap to start",
+            fontWeight = FontWeight.Bold,
+            fontSize = 20.sp,
+            color = Color.White,
+            modifier = Modifier.padding(36.dp, 12.dp)
+        )
+    }
+}
+
+@Composable
+fun ViewResultHistoryButton() {
+    Surface(
+        color = Color.Gray.copy(alpha = 0.65f),
+    ) {
+        Box(
+            modifier = Modifier.padding(16.dp, 0.dp)
+        ) {
+            Row(
+            ) {
+                HistoryIcon(iconColor = Color.White, iconSize = 48.dp)
+                Text(
+                    text = "リザルト履歴確認",
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 20.sp,
+                    color = Color.White,
+                    modifier = Modifier.padding(12.dp, 12.dp)
+                )
+            }
         }
     }
 }
