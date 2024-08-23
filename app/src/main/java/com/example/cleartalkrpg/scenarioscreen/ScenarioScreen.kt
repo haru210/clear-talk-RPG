9a92643d296203b9b15fa1e6806eea30239ab530
nashdev255 <nashdev255@gmail.com>
2024-08-24 00:13:58 +0900

    Adjust ScenarioScreen layout

--- app/src/main/java/com/example/cleartalkrpg/scenarioscreen/ScenarioScreen.kt
+++ app/src/main/java/com/example/cleartalkrpg/scenarioscreen/ScenarioScreen.kt
@@ -1,2 +1,126 @@
 package com.example.cleartalkrpg.scenarioscreen
 
+import android.graphics.Paint.Align
+import android.view.Display
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+import com.example.cleartalkrpg.R
+import com.example.cleartalkrpg.scenarioselectscreen.Scenario
+import kotlinx.coroutines.delay
+
+@Composable
+fun ScenarioScreen(navController: NavController, selectedScenarioId: Int) {
+    TitleScreenBackgroundImage()
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(0.dp, 24.dp),
+        contentAlignment = Alignment.BottomCenter
+    ) {
+        ScenarioMessageBox(
+            scenarioMessage =
+                    "おう　なつだぜ\n" +
+                    "おれは　げんきだぜ\n" +
+                    "あまり　ちかよるな",
+            scenarioCharacterName = "かまきり"
+        )
+    }
+}
+
+/* タイトル画面の背景を表示する関数 */
+@Composable
+fun TitleScreenBackgroundImage() {
+    Box(modifier = Modifier.fillMaxSize()) {
+        Image(
+            painter = painterResource(id = R.drawable.title_screen_background_image),
+            contentDescription = "Title screen background image",
+            contentScale = ContentScale.FillBounds,
+            modifier = Modifier.matchParentSize()
+        )
+    }
+}
+
+@Composable
+fun ScenarioMessageBox(scenarioMessage: String, scenarioCharacterName: String) {
+    Column(
+
+    ) {
+        ScenarioCharacterNamePlate(characterName = scenarioCharacterName)
+        DisplayScenarioMessage(scenarioMessage = scenarioMessage)
+    }
+}
+
+@Composable
+fun ScenarioCharacterNamePlate(characterName: String) {
+    Surface(
+        color = Color.Gray.copy(0.65f),
+        modifier = Modifier
+            .padding(12.dp, 0.dp)
+            .clip(RoundedCornerShape(8.dp))
+    ) {
+        Text(
+            text = characterName,
+            color = Color.White,
+            fontSize = 18.sp,
+            modifier = Modifier.padding(16.dp, 4.dp)
+        )
+    }
+}
+
+@Composable
+fun DisplayScenarioMessage(scenarioMessage: String) {
+    var displayedMessage by remember { mutableStateOf("") }
+
+    // LaunchedEffectで文字を1文字ずつ表示する
+    LaunchedEffect(scenarioMessage) {
+        displayedMessage = ""
+        for (i in scenarioMessage.indices) {
+            delay(50)
+            displayedMessage += scenarioMessage[i]
+        }
+    }
+
+    Surface(
+        color = Color.Gray.copy(0.65f),
+        modifier = Modifier
+            .height(100.dp)
+            .width(650.dp)
+            .clip(RoundedCornerShape(8.dp))
+    ) {
+        Text(
+            text = displayedMessage,
+            color = Color.White,
+            fontSize = 18.sp,
+            modifier = Modifier.padding(28.dp, 12.dp)
+        )
+    }
+}
