c7f4f7a8db254cab7f9a43cb00ebcc42bff62207
haru210 <miroku.pr@outlook.jp>
2024-08-29 15:46:02 +0900

    make feature to calculate clarity

--- app/src/main/java/com/example/cleartalkrpg/scenarioscreen/SpeechRecognizer.kt
+++ app/src/main/java/com/example/cleartalkrpg/scenarioscreen/SpeechRecognizer.kt
@@ -23,6 +23,33 @@ fun SpeechRecognize(context: Context) {
             }
         }
     )
+    var target : String = "test"
+    var res : String = speechRecognizerManager.speechResult.value
+    val tar_length = target.length
+    val res_length = res.length
+    //編集距離を求める
+    var dp = Array(tar_length + 1) { IntArray(res_length + 1) }
+
+    for(i in 0..tar_length) {
+        dp[i][0] = i
+    }
+    for(j in 0..res_length) {
+        dp[0][j] = j
+    }
+
+    for(i in 1..tar_length) {
+        for(j in 1..res_length) {
+            val cost = if(target[i - 1] == res[j - 1]) 0 else 1
+            dp[i][j] = minOf(
+                dp[i-1][j] + 1,
+                dp[i][j-1] + 1,
+                dp[i-1][j-1] + cost
+                )
+        }
+    }
+    val lev_dis = dp[tar_length][res_length]
+    val clarity : Int = 40 - lev_dis * 2
+
 }
 
 class SpeechRecognizerManager(private val context: Context) {
