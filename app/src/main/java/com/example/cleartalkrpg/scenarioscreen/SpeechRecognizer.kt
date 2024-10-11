74bdd31241a8251704960aedbc2916b38421a43a
haru210 <miroku.pr@outlook.jp>
2024-10-11 22:35:59 +0900

    fixed feature to calculate volumeScore

--- app/src/main/java/com/example/cleartalkrpg/scenarioscreen/SpeechRecognizer.kt
+++ app/src/main/java/com/example/cleartalkrpg/scenarioscreen/SpeechRecognizer.kt
@@ -23,8 +23,7 @@ class SpeechRecognizerManager(private val context: Context) {
     var startTime : Long = 0
     var endTime : Long = 0
     var speechDuration : Long = 0
-    var beforeTime : Long = 0
-    val volumeList = ArrayList<Pair<Long, Float>>()
+    val volumeList = ArrayList<Float>()
     var speedScore : Int = 30
     var clarityScore : Int = 40
     var volumeScore : Int = 0
@@ -44,13 +43,7 @@ class SpeechRecognizerManager(private val context: Context) {
                     startTime = System.currentTimeMillis()
                 }
                 override fun onRmsChanged(rmsdB: Float) {
-                    if(beforeTime == 0L){
-                        beforeTime = System.currentTimeMillis()
-                    }
-                    else if(rmsdB > 0) {
-                        volumeList.add(Pair(System.currentTimeMillis() - beforeTime, rmsdB))
-                        beforeTime = System.currentTimeMillis()
-                    }
+                    volumeList.add(rmsdB)
                 }
                 override fun onBufferReceived(buffer: ByteArray?) {}
                 override fun onEndOfSpeech() {
@@ -138,9 +131,11 @@ class SpeechRecognizerManager(private val context: Context) {
         var volumeAvg: Double = 0.0
         //音量の点数を求める
         for (i in 0..volumeList.size - 1) {
-            volumeAvg += volumeList[i].second.toDouble() * ((volumeList[i].first.toDouble() / speechDuration.toDouble()) / 1000.0)
+            volumeAvg += volumeList[i].toDouble()
         }
-        volumeScore = Math.round(volumeAvg * 3.0).toInt()
+        volumeAvg /= volumeList.size
+        if(volumeAvg > 10.0) volumeAvg = 10.0
+        volumeScore = Math.round(volumeAvg).toInt()
         val result = speechResult.value
         return Triple(speedScore, clarityScore, volumeScore)
     }
