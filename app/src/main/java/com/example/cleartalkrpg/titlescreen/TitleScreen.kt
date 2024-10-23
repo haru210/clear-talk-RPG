d57b9e37847b01bdbc4fab952bf594cd59c19f76
nashdev255 <nashdev255@gmail.com>
2024-10-23 20:27:38 +0900

    Adjust TitleScreen

--- app/src/main/java/com/example/cleartalkrpg/titlescreen/TitleScreen.kt
+++ app/src/main/java/com/example/cleartalkrpg/titlescreen/TitleScreen.kt
@@ -1,41 +1,24 @@
 package com.example.cleartalkrpg.titlescreen
 
-import android.Manifest.permission.RECORD_AUDIO
-import androidx.activity.compose.rememberLauncherForActivityResult
-import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.IntrinsicSize
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.material3.Text
-import androidx.compose.ui.unit.dp
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Surface
 import androidx.compose.ui.Alignment
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import com.example.cleartalkrpg.R
-import com.example.cleartalkrpg.ui.theme.HistoryIcon
 import com.example.cleartalkrpg.ClearTalkRPGScreen
-import com.example.cleartalkrpg.ui.theme.TapEffectScreen
 
 @Composable
 fun TitleScreen(navController: NavController) {
@@ -43,31 +26,62 @@ fun TitleScreen(navController: NavController) {
         modifier = Modifier
             .clickable { navController.navigate(ClearTalkRPGScreen.Home.name) }
     ) {
-        Column {
-            TitleScreenBackgroundImage()
-            TitleLogo()
+        TitleScreenBackgroundImage()
+        Box(
+            contentAlignment = Alignment.Center,
+            modifier = Modifier.fillMaxSize()
+        ) {
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                TitleLogo()
+                TapToStart()
+            }
         }
     }
 }
 
+/* タイトルロゴ */
 @Composable
 fun TitleLogo() {
-    Text(
-        text = "ClearTalk RPG",
-        fontFamily = FontFamily(Font(R.font.koruri_bold)),
-        fontSize = 40.sp
-    )
+    Box {
+        Text(
+            text = "ClearTalk RPG",
+            fontFamily = FontFamily(Font(R.font.hangyaku)),
+            fontSize = 100.sp,
+            color = Color.White,
+            fontWeight = FontWeight(1000)
+        )
+        Text(
+            text = "ClearTalk RPG",
+            fontFamily = FontFamily(Font(R.font.hangyaku)),
+            fontSize = 100.sp,
+            color = Color.Black
+        )
+    }
 }
 
+/* タイトルロゴ下部の画面タップを促すテキスト */
 @Composable
-fun TapToStartButton() {
-    Text(
-        text = "tap to start",
-        fontFamily = FontFamily(Font(R.font.koruri_bold)),
-        fontSize = 16.sp
-    )
+fun TapToStart() {
+    Box {
+        Text(
+            text = "tap to start",
+            fontFamily = FontFamily(Font(R.font.hangyaku)),
+            fontSize = 30.sp,
+            color = Color.White,
+            fontWeight = FontWeight(1000)
+        )
+        Text(
+            text = "tap to start",
+            fontFamily = FontFamily(Font(R.font.hangyaku)),
+            fontSize = 30.sp,
+            color = Color.Black
+        )
+    }
 }
 
+/* 背景 */
 @Composable
 fun TitleScreenBackgroundImage() {
     Box(modifier = Modifier.fillMaxSize()) {
